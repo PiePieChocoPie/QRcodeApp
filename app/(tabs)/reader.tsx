781635f960bde03cb3a89b755399969c21768ff5
Arm <Arm@tmn.martinural.ru>
2024-04-10 12:09:53 +0500

    refact camera to next route

--- app/(tabs)/reader.tsx
+++ app/(tabs)/reader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Text, View, TouchableOpacity, Alert, ActivityIndicator} from "react-native";
-import { Camera } from "expo-camera";
+import { Button, StyleSheet, Text, TouchableOpacity, View,Alert } from 'react-native';
+import { CameraView,useCameraPermissions  } from "expo-camera/next";
 import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 import ChooseStateDialog from "src/modals/chooseStateDialog";
 import Svg,{Polyline} from "react-native-svg";
@@ -9,16 +9,19 @@ import { projColors, styles } from "src/stores/styles";
 import Store from "src/stores/mobx";
 import { useFocusEffect } from "expo-router";
 import useLoading from "src/useLoading";
+import { useState } from 'react';
+
 
 export default function Reader() {
-    const [hasPermission, setHasPermission] = React.useState(null);
+    const [permission, requestPermission] = useCameraPermissions();
     const [scanned, setScanned] = React.useState(false);
     const [modalVisibleState, setModalVisibleState] = React.useState(false);
     const [modalText, setModalText] = React.useState('');
     const [docNumber, setDocNumber] = React.useState(0);
     const {loading, startLoading, stopLoading} = useLoading()
+    
+
     useFocusEffect(
-        
         React.useCallback(() => {
         const fetchData = async () => { 
                 
@@ -28,6 +31,7 @@ export default function Reader() {
                 .then(async (res) => {
                     if(res.status)  
                     {
+
                     }
                     else 
                         Alert.alert("Ошибка", res.curError);
@@ -46,16 +50,8 @@ export default function Reader() {
         }, []) 
     );
 
-    React.useEffect(() => {
-        const getCameraPermissions = async () => {
-            const { status } = await Camera.requestCameraPermissionsAsync();
-            return status === "granted";
-        };
-        getCameraPermissions().then(setHasPermission);
 
-    }, []);
-
-     const handleBarCodeScanned = async ({ data }) => {
+    const handleBarCodeScanned = async ({ data }) => {
         try{
             setScanned(true)
             await getDataAboutDocs(data)
@@ -76,11 +72,11 @@ export default function Reader() {
                 }
             })
             .catch((err) =>{
-                //console.log(err);
+                console.log(err);
             })
         }
         catch(e){
-            //console.log(`ошибка получения документа - ${e}`);
+            console.log(`ошибка получения документа - ${e}`);
         }
      };
 
@@ -88,80 +84,30 @@ export default function Reader() {
         setModalVisibleState(!modalVisibleState);
         setScanned(!scanned)
     };
+    
 
-    if (hasPermission === null) {
-        return <View style={styles.container}><Text style={styles.text}>Запрос на использование камеры</Text></View>;
-    }
-    if (hasPermission === false) {
-        return <View style={styles.container}><Text style={styles.text}>Нет доступа к камере</Text></View>;
-    }
+    
+
+  
+    return (
+
+      <View style={styles.overlay}>
+
+        <CameraView style={styles.camera} facing={'back'} onBarcodeScanned={scanned ? undefined : handleBarCodeScanned}>
+
+            <View>
 
 
+            {scanned && (
+                <TouchableOpacity style={styles.opacities} onPress={() => setScanned(false)}>
+                    <Icon2 name="refresh"  size={40} color="#fff"/>
+                    <Text style={styles.text}>сканировать снова</Text>
+                </TouchableOpacity>
+            )}
+            </View>
+            <ChooseStateDialog visible={modalVisibleState} onClose={toggleModalState}  docData={modalText} docNumber={docNumber}/>
+        </CameraView>
 
-    return <View style={styles.container}>
-        {loading ?
-            (
-            <View style={styles.containerCentrallity}>
-                <ActivityIndicator size="large" color={projColors.currentVerse.fontAccent} />
-            </View> 
-            ):
-           
-        (
-        <View style={styles.overlay} >
-                <Camera style={styles.camera} onBarCodeScanned={scanned ? undefined : handleBarCodeScanned} >
-                {/* Верхняя часть: камера */}
-                    <View style={{flex:1}}>
-                        <View style={styles.overlay}></View>
-                    <View style={styles.horizontalBorders}>
-                        <View style={styles.overlay} />
-                            <View style={styles.cameraContainer}>
-                                <Svg    width="100%"
-                                        height="100%"
-                                        viewBox={'0 0 60 60'}
-                                        style={{overflow:'visible'}}
-                                >
-                                    <Polyline
-                                        points="0,20 0,0 20,0"
-                                        fill="none"
-                                        stroke="white"
-                                        strokeWidth="6"
-                                    />
-                                    <Polyline
-                                        points="40,0 60,0 60,20"
-                                        fill="none"
-                                        stroke="white"
-                                        strokeWidth="6"
-                                    />
-                                    <Polyline
-                                        points="0,40 0,60 20,60"
-                                        fill="none"
-                                        stroke="white"
-                                        strokeWidth="6"
-                                    />
-                                    <Polyline
-                                        points="40,60 60,60 60,40"
-                                        fill="none"
-                                        stroke="white"
-                                        strokeWidth="6"
-                                    />
-                                </Svg>
-                            </View>
-                            <View style={styles.overlay} />
-                            </View>
-                            <View style={styles.overlay} >
-                            {scanned && (
-                            <TouchableOpacity style={styles.opacities} onPress={() => setScanned(false)}>
-                                <Icon2 name="refresh"  size={40} color="#fff"/>
-                                <Text style={styles.text}>сканировать снова</Text>
-                            </TouchableOpacity>
-                            )}
-                        </View>
-                    <ChooseStateDialog visible={modalVisibleState} onClose={toggleModalState}  docData={modalText} docNumber={docNumber}/>
-                </View>
-            </Camera>
-        </View>
-        )
-    }    
-    </View>
-}
-
+      </View>
+    );
+  }
