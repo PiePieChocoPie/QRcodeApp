052fad1d2391ec171701d5eb482cd57a96f389b3
Arm <Arm@tmn.martinural.ru>
2024-06-17 16:14:07 +0500

    clear

--- assets/navbar_icons/index.js
+++
@@ -1,6 +0,0 @@
-export { default as arrow } from './arrow.svg';
-export { default as calendar } from './calendar.svg';
-export { default as list } from './list.svg';
-export { default as profile } from './profile.svg';
-export { default as qr } from './qr.svg';
-export { default as tasks } from './tasks.svg';
