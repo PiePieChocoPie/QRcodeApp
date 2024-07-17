757bf175e6b856271228f240dfc67715088b8149
Денис Д. Шмелёв <ShmelyovDD@tmn.martinural.ru>
2024-07-17 16:54:44 +0500

    Fix filter

--- app/(tabs)/tasks.tsx
+++ app/(tabs)/tasks.tsx
@@ -19,14 +19,15 @@ const Tasks = () => {
     const scrollViewRef = useRef(null);
 
     const data = [
-        { id: '0', status: 'все', colors: [projColors.currentVerse.border, projColors.currentVerse.fontAccent] },
-        { id: '1', status: 'просрочены', colors: ['#FF5752', '#FFFFFF'] },
-        { id: '2', status: 'на сегодня', colors: ['#9DCF00', '#535D69'] },
-        { id: '3', status: 'на этой неделе', colors: ['#2FC6F6', '#535D69'] },
-        { id: '4', status: 'на следующей неделе', colors: ['#55D0E0', '#535D69'] },
-        { id: '5', status: 'без срока', colors: ['#A8ADB4', '#535D69'] },
-        { id: '6', status: 'больше двух недель', colors: ['#468EE5', '#FFFFFF'] },
+        { id: '0', status: 'все', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '1', status: 'просрочены', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '2', status: 'на сегодня', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '3', status: 'на этой неделе', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '4', status: 'на следующей неделе', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '5', status: 'без срока', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
+        { id: '6', status: 'больше двух недель', colors: ['#83AD00', '#FFFFFF'] },  // зеленый фон и белый текст
     ];
+    
 
     useFocusEffect(
         React.useCallback(() => {
@@ -103,13 +104,13 @@ const Tasks = () => {
                 >
                     <View style={[styles.expanderContainer, { backgroundColor: "transparent", marginTop: '10%' }]}>
                         <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-                            <Pressable onPress={() => focusChange('Tasks')} style={[styles.listElementContainer, { width: '40%', alignItems: "center", flex: 1 }, selectedList == 'Tasks' ? { backgroundColor: projColors.currentVerse.extra } : { backgroundColor: projColors.currentVerse.extrasecond }]}>
+                            <Pressable onPress={() => focusChange('Tasks')} style={[styles.listElementContainer, { width: '40%', alignItems: "center", flex: 1 }, selectedList === 'Tasks' ? { backgroundColor: projColors.currentVerse.extra } : { backgroundColor: projColors.currentVerse.extrasecond }]}>
                                 <Svg width="17" height="20" viewBox="0 0 17 20" fill="none">
                                     <Path d="M7.45 16L13.1 10.35L11.65 8.9L7.425 13.125L5.325 11.025L3.9 12.45L7.45 16ZM2.5 20C1.95 20 1.47933 19.8043 1.088 19.413C0.696666 19.0217 0.500667 18.5507 0.5 18V2C0.5 1.45 0.696 0.979333 1.088 0.588C1.48 0.196667 1.95067 0.000666667 2.5 0H10.5L16.5 6V18C16.5 18.55 16.3043 19.021 15.913 19.413C15.5217 19.805 15.0507 20.0007 14.5 20H2.5ZM9.5 7H14.5L9.5 2V7Z" fill={selectedList === 'Tasks' ? projColors.currentVerse.extrasecond : projColors.currentVerse.extra} />
                                 </Svg>
                                 <Text style={[styles.Title, selectedList === 'Tasks' ? { color: projColors.currentVerse.extrasecond } : { color: projColors.currentVerse.extra }]}>Задачи</Text>
                             </Pressable>
-                            <Pressable onPress={() => focusChange('Attorney')} style={[styles.listElementContainer, { width: '40%', alignItems: "center", flex: 1 }, selectedList == 'Attorney' ? { backgroundColor: projColors.currentVerse.extra } : { backgroundColor: projColors.currentVerse.extrasecond }]}>
+                            <Pressable onPress={() => focusChange('Attorney')} style={[styles.listElementContainer, { width: '40%', alignItems: "center", flex: 1 }, selectedList === 'Attorney' ? { backgroundColor: projColors.currentVerse.extra } : { backgroundColor: projColors.currentVerse.extrasecond }]}>
                                 <Svg width="22" height="23" viewBox="0 0 22 23">
                                     <Path d="M4.5 0H14.5V2H18.5V11H16.5V4H14.5V6H4.5V4H2.5V20H9.5V22H0.5V2H4.5V0ZM6.5 4H12.5V2H6.5V4ZM17.5 13V17H21.5V19H17.5V23H15.5V19H11.5V17H15.5V13H17.5Z" fill={selectedList === 'Attorney' ? projColors.currentVerse.extrasecond : projColors.currentVerse.extra} />
                                 </Svg>
@@ -118,18 +119,28 @@ const Tasks = () => {
                         </View>
                         {selectedList === "Tasks" && (
                             <FlatList
-                                data={data}
-                                keyExtractor={item => item.id}
-                                horizontal
-                                renderItem={({ item }) =>
-                                    <Pressable onPress={() => setFilterIndex(item.id)}>
-                                        <View style={[styles.listElementContainer, { backgroundColor: item.colors[0], padding: 15, margin: 7 }]}>
-                                            <Text style={[styles.Title, { color: item.colors[1] }]}>{item.status}</Text>
-                                        </View>
-                                    </Pressable>
-                                }
-                                showsHorizontalScrollIndicator={false}
-                            />
+                            data={data}
+                            keyExtractor={item => item.id}
+                            horizontal
+                            renderItem={({ item }) =>
+                                <Pressable onPress={() => setFilterIndex(item.id)}>
+                                    <View style={[
+                                        styles.listElementContainer,
+                                        { backgroundColor: filterIndex === item.id ? item.colors[0] : '#FFFFFF' }, // Установка цвета по умолчанию, например, белый
+                                        { padding: 15, margin: 7 }
+                                    ]}>
+                                        <Text style={[
+                                            styles.Title,
+                                            { color: filterIndex === item.id ? item.colors[1] : '#000000' } // Установка цвета текста по умолчанию, например, черный
+                                        ]}>
+                                            {item.status}
+                                        </Text>
+                                    </View>
+                                </Pressable>
+                            }
+                            showsHorizontalScrollIndicator={false}
+                        />
+                        
                         )}
                     </View>
                     <View style={styles.container}>
@@ -145,17 +156,17 @@ const Tasks = () => {
                 </ScrollView>
 
                 <Pressable
-                onPress={() => scrollViewRef.current?.scrollTo({ y: 0, animated: true })}
-                style={({ pressed }) => [
-                    localStyles.scrollToTopButton,
-                    {
-                        backgroundColor: pressed ? projColors.currentVerse.redroDark : projColors.currentVerse.redro,
-                    },
+                    onPress={() => scrollViewRef.current?.scrollTo({ y: 0, animated: true })}
+                    style={({ pressed }) => [
+                        localStyles.scrollToTopButton,
+                        {
+                            backgroundColor: pressed ? projColors.currentVerse.redroDark : projColors.currentVerse.redro,
+                        },
                     ]}
-                    >
-                        <Text style={localStyles.scrollToTopButtonText}>↑</Text>
+                >
+                    <Text style={localStyles.scrollToTopButtonText}>↑</Text>
                 </Pressable>
-                
+
             </View>
         )
     );
